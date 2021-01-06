d329c7d0b74849fc7d57db217f967642afabb3aa
Chandan Satija <chandan.satija@mail.concordia.ca>
2021-01-06 12:19:28 -0500

    Added .gitignore

---
+++ Case I/Ray Tracing_Case1.cpp
@@ -0,0 +1,210 @@
+#define _USE_MATH_DEFINES // Marco for Pi
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+#include "Ray_Tracing_Rev3.h"
+
+using namespace std;
+
+//Initliaze ray counter
+long int ray::ray_count = 0;
+int source::source_count = 0;
+
+int main()
+{
+	//Timer
+	auto start = std::chrono::steady_clock::now();
+
+	//Generate Room 
+	box room;
+	room.width = 4;
+	room.length = 4;
+	room.height = 4;
+
+	// Generates Array of planes according to room dimension
+	room.generate_box_room();
+	int no_of_planes = 6;
+
+	complex R;
+	R.real = 1;
+	R.imaginary = 0;
+
+	//Room Surface Refelction Coefficient
+	room.box_plane[0].R_coeff.real = R.real;//Bottom
+	room.box_plane[0].R_coeff.imaginary = R.imaginary;
+	room.box_plane[1].R_coeff.real = 0.0;//left
+	room.box_plane[1].R_coeff.imaginary = 0;
+	room.box_plane[2].R_coeff.real = 0.0;//front
+	room.box_plane[2].R_coeff.imaginary = 0;
+	room.box_plane[3].R_coeff.real = 0.0; //right
+	room.box_plane[3].R_coeff.imaginary = 0;
+	room.box_plane[4].R_coeff.real = 0.0;//back
+	room.box_plane[4].R_coeff.imaginary = 0;
+	room.box_plane[5].R_coeff.real = 0.0;//top
+	room.box_plane[5].R_coeff.imaginary = 0;
+
+	int reflecting_plane_no;
+	
+	//Generate Source
+	source source1;
+	source1.number_of_rays = 1*1.0e6;
+	source1.position.x = room.length / 2.0;
+	source1.position.y = room.width / 2.0;
+	source1.position.z = 3.0;
+
+	source1.power_db = 100;
+	source1.frequency = 250;//Hz
+
+	//Termination Criteria
+	int reflection_order = 1;
+	double threshold=0.0;
+
+	source1.generate_source();
+
+	//Generate Reciever
+	receiver receiver1;
+	receiver1.position.x = 0;
+	receiver1.position.y = 0;
+	receiver1.position.z = 0;
+	receiver1.radius = .025;
+
+	//For Plotting
+	ofstream ray_tracing_plot;
+	int progress = 10;
+	int progress_count = 0;
+
+	ray_tracing_plot.open("Case1_.025_one_wall_250Hz_3mil_100db_cube_analytical_complexR_1_0.csv");
+
+	//Reciever Movement Variables
+	double d_step = .05;
+	double d_max = 3.0-d_step;
+	double d_min = 0.0;
+	
+	cout << "Position of source \n";
+	cout << source1.position.x << "\t" << source1.position.y << "\t" << source1.position.z << "\n";
+	cout << "Source Power (Watts) \t" << source1.power << "\n";
+	cout << "Source Frequency (Hz) \t" << source1.frequency << "\n";
+	cout << "\n************************** \nRunning Ray Tracing Algorithm : Case 1\n";
+
+
+	for (double d=d_min+d_step;d < d_max;d+=d_step)
+	{ 
+		receiver1.position.x = source1.position.x;
+		receiver1.position.y = source1.position.y;
+		receiver1.position.z = d;
+
+		//Reset All Rays 
+		source1.reset_rays();
+
+		//Reset Receiver
+		receiver1.reset();
+
+		//Reset Counter
+		progress_count++;
+
+		for (unsigned int ray_no = 0; ray_no < source1.number_of_rays; ray_no++)
+		{
+			if (source1.rays[ray_no].R_eff.real == 0 && source1.rays[ray_no].R_eff.imaginary == 0) continue; // Move to next ray
+
+			for (int r = 1; r <= reflection_order+1; r++)
+			{
+				if (source1.rays[ray_no].R_eff.real == 0 && source1.rays[ray_no].R_eff.imaginary == 0)
+				{
+					break; // Terminate Ray
+				}
+				else if (detect_receiver(source1, receiver1, ray_no))
+				{
+					complex ray_pressure = calculate_ray_pressure(source1, receiver1, ray_no);
+
+					double R_eff_magnitude = sqrt(source1.rays[ray_no].R_eff.real*source1.rays[ray_no].R_eff.real + source1.rays[ray_no].R_eff.imaginary*source1.rays[ray_no].R_eff.imaginary);
+
+					if (R_eff_magnitude <= threshold) {
+						break; //Terminate Ray
+					}
+
+					else {
+						receiver1.pressure = receiver1.pressure + ray_pressure;
+					}
+				}
+
+				// Ray Reflection
+
+				// Find Reflection Plane
+				reflecting_plane_no = find_reflecting_box_plane(room, source1, ray_no);
+
+				// Get source image
+				vec3 image = image_source_position(source1.rays[ray_no].isource_position, room.box_plane[reflecting_plane_no]);
+
+				// Get reflection point
+				vec3 rp = reflection_point(source1.rays[ray_no].isource_position, room.box_plane[reflecting_plane_no], source1.rays[ray_no]);
+
+				// Get new direction
+				vec3 reflected = reflected_ray(room.box_plane[reflecting_plane_no], source1.rays[ray_no]);
+
+				//Get Angle
+				double angle_of_reflection = reflection_angle(room.box_plane[reflecting_plane_no], source1.rays[ray_no]);
+
+				//Calculate Reflection Coeff for the surface
+				complex Rsurface = calculate_R(room.box_plane[reflecting_plane_no], angle_of_reflection);
+
+				// Get new ray after reflection (update ray)
+				source1.rays[ray_no].isource_position = image;
+				source1.rays[ray_no].direction = reflected;
+				source1.rays[ray_no].R_eff = source1.rays[ray_no].R_eff*Rsurface;
+			}
+		}
+
+		double W_0 = source1.power;
+		double rho = 1.2041;
+		double c = 343.22;
+
+		//Case 1 
+		double r0 = sqrt((receiver1.position.z - source1.position.z)*(receiver1.position.z - source1.position.z));
+		double r1 = sqrt((receiver1.position.z + source1.position.z)*(receiver1.position.z + source1.position.z));
+
+		double k = (2*PI) / source1.wavelength;
+
+		double p2_analytical = ((W_0*rho*c) / (4 * PI))*(((R.real*R.real + R.imaginary*R.imaginary) / (r1*r1)) + (1.0 / (r0*r0)) + (sqrt(R.real*R.real + R.imaginary*R.imaginary)*(2 * cos(k*(r1 - r0)+atan2(R.imaginary,R.real))) / (r0*r1)));
+
+		double p0 = 2.0e-5;
+
+		double analytical_pressure_db = 10 * log10(p2_analytical / (p0*p0));
+		
+		ray_tracing_plot << d << "," <<receiver_pressure_db(receiver1) <<"," << receiver1.detect_count <<"," << analytical_pressure_db <<"\n";
+		
+
+		if ( progress_count>=(progress/100.0)*((d_max - d_min) / d_step))
+		{
+			cout << "Progress = " << progress << " % \n";
+			progress += 10;
+		}
+
+	}
+
+	ray_tracing_plot.close();
+
+	source1.delete_source();
+
+	//Program Timer
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double,std::milli> elapsed_seconds = end - start;
+	double total_time_milli = elapsed_seconds.count();
+	double total_time = elapsed_seconds.count()/(1000.0);
+	int hrs = (int)(total_time / (60.0 * 60.0));
+	int minutes = (int)((total_time - hrs * 60.0 * 60.0) / 60.0);
+	int seconds = (int)((total_time - hrs * 60.0 * 60.0 - minutes * 60.0));
+	int millis = (int)((total_time_milli - hrs * 60.0 * 60.0*1000.0 - minutes * 60.0*1000.0-seconds*1000.0));
+	std::cout << "elapsed time: " << hrs << "\t hrs \t" << minutes << "\t min \t" << seconds << "\t seconds \t" << millis <<" milliseconds \t" << "\n";
+
+	cout << "Program Complete \n";
+	cout << "Press Enter key to close program";
+
+	getchar();
+
+	return 0;
+}
+
---
+++ Case II/Ray_Tracing_Rev3.h
@@ -0,0 +1,556 @@
+#define _USE_MATH_DEFINES 
+
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <memory>
+
+using namespace std;
+
+static double PI = M_PI; // Marco for Pi
+
+// -----------------Declerations ---------------------
+
+// Object class declerations
+class complex; // Complex Number 
+class vec3; //3-Dimensional Vector
+class ray; // Ray object
+class source; //Source object
+class plane; // Plane object
+class box; //Shoe-box room object
+class receiver; //Receiver object
+
+//Vector function declerations
+double dotProduct(vec3 vect_A, vec3 vect_B);
+vec3 crossProduct(vec3 vect_A, vec3 vect_B);
+double mod_vect(vec3 vect);
+
+//Room generation function declerations
+plane generate_plane(vec3 point1, vec3 point2, vec3 point3);
+
+//Ray_tracing functions declerations
+vec3 image_source_position(vec3 source, plane plane);
+vec3 reflection_point(vec3 source, plane plane, ray current_ray);
+double reflection_angle(plane plane, ray current_ray);
+vec3 reflected_ray(plane plane, ray current_ray);
+bool check_reflecting_plane(vec3 source, plane plane);
+int find_reflecting_box_plane(box room, source source, int ray_no);
+complex calculate_ray_pressure(source source, receiver receiver, int ray_no);
+complex calculate_R(plane reflecting_plane,double angle_of_reflection);
+
+//Reciever functions declerations
+bool detect_receiver(source &source, receiver &receiver, int ray_no);
+double receiver_pressure_db(receiver receiver);
+
+// -----------------Class Definations ---------------------
+
+class complex
+{
+public:
+	float real;
+	float imaginary;
+
+	complex operator + (const complex& B) {
+		complex sum;
+		sum.real = this->real + B.real;
+		sum.imaginary = this->imaginary + B.imaginary;
+		return sum;
+	}
+
+	complex operator * (const complex& B) {
+		complex mul;
+		mul.real = (this->real) * B.real - (this->imaginary) * B.imaginary;
+		mul.imaginary = (this->imaginary) * B.real + (this->real) * B.imaginary;
+		return mul;
+	}
+};
+
+class vec3
+{
+public:
+	float x;
+	float y;
+	float z;
+
+	vec3 operator + (const vec3& vectB) {
+		vec3 sum;
+		sum.x = this->x + vectB.x;
+		sum.y = this->y + vectB.y;
+		sum.z = this->z + vectB.z;
+		return sum;
+	}
+
+	vec3 operator - (const vec3& vectB) {
+		vec3 diff;
+		diff.x = this->x - vectB.x;
+		diff.y = this->y - vectB.y;
+		diff.z = this->z - vectB.z;
+		return diff;
+	}
+
+	vec3 operator * (const double& B) {
+		vec3 mul;
+		mul.x = (this->x) * (float)B;
+		mul.y = (this->y) * (float)B;
+		mul.z = (this->z) * (float)B;
+		return mul;
+	}
+
+	vec3 operator / (const double& B) {
+		vec3 div;
+		div.x = (this->x) / (float)B;
+		div.y = (this->y) / (float)B;
+		div.z = (this->z) / (float)B;
+		return div;
+	}
+};
+
+class plane
+{
+public:
+	vec3 normal;
+	vec3 point;
+	double D;
+
+	complex R_coeff;
+
+	bool label = 0;
+};
+
+class box 
+{
+public:
+	float width, length, height;
+	plane box_plane[6];
+	void generate_box_room();
+	box();
+};
+
+box::box() {
+	width = 0; length = 0; height = 0;
+}
+
+void box::generate_box_room()
+{
+
+	//generate corner Points
+	vec3 c1, c2, c3, c4, c5, c6, c7, c8;
+	/* ShoeBox Corners
+	  c6------------c8
+	 /	|		   / |
+	c5------------c7 |
+	|  c2---------|-c4
+	| /		      | /
+	c1----------- c3
+
+	Upward : Positive Z Direction
+	Right : Positive X Direction 
+	Forward : Positive Y Direction
+	Origin at corner c1
+	*/
+	c1.x = 0; c1.y = 0; c1.z = 0;
+	c2.x = 0; c2.y = width; c2.z = 0;
+	c3.x = length; c3.y = 0; c3.z = 0;
+	c4.x = length; c4.y = width; c4.z = 0;
+	c5.x = 0; c5.y = 0; c5.z = height;
+	c6.x = 0; c6.y = width; c6.z = height;
+	c7.x = length; c7.y = 0; c7.z = height;
+	c8.x = length; c8.y = width; c8.z = height;
+
+	//Generate all planes 
+	int no_of_planes = 6;
+
+	box_plane[0] = generate_plane(c1, c3, c2);//bottom
+	box_plane[1] = generate_plane(c1, c2, c5);//left
+	box_plane[2] = generate_plane(c1, c5, c7);//front
+	box_plane[3] = generate_plane(c3, c7, c8);//right
+	box_plane[4] = generate_plane(c4, c8, c6);//back
+	box_plane[5] = generate_plane(c7, c5, c6);//top
+
+	cout << "Shoe-box room environment generated \n";
+}
+
+class ray
+{
+public:
+	float theta, phi;
+	vec3 direction;
+	vec3 isource_position;
+	static long int ray_count;
+	complex R_eff;
+	vec3 calc_pt;
+	double pressure_square;
+
+	ray(void);
+	~ray(void);
+};
+
+ray::ray(void)
+{
+	ray_count++;
+}
+
+ray::~ray(void)
+{
+	ray_count--;
+}
+
+class source
+{
+public:
+	static int source_count;
+	unsigned long int number_of_rays = 0;
+	double power_db; // Source Power in Decibels
+	double power; // Source Power in Watts
+	double A;
+	double wavelength;
+	double frequency;
+	vec3 position;
+	ray* rays; // Pointer to array of class ray 
+
+	void generate_source(void);
+	void delete_source(void);
+	void reset_rays(void);
+	source(void);
+	~source(void);
+};
+
+void source::generate_source(void)
+{
+	double power_ref = 1.0e-12;
+	double rho = 1.2041;
+	double c = 343.22;
+
+	//Source Characteristics
+	power = power_ref * pow(10, (power_db / 10.0));
+
+	A = sqrt((rho*c*(power/number_of_rays))/(4*PI));
+	
+	wavelength = c / frequency;
+
+	rays = new ray[number_of_rays];
+
+	unsigned int ray_no;
+
+	//Generate a random ray directions
+
+	//Random Number Generator
+	random_device seed; // Seed to make randon number underministic
+	mt19937 generator(seed()); // Algorithm for random number
+	uniform_real_distribution<float> uniform_random(0.0, 1.0); //Distribution Type and range
+	
+    //Initialize Rays 
+	for (ray_no = 0; ray_no < number_of_rays; ray_no++) {
+
+		//Ray source Position
+		rays[ray_no].isource_position = this->position;
+
+		//Ray Initial Reflection Coefficient
+		rays[ray_no].R_eff.real = 1.0;
+		rays[ray_no].R_eff.imaginary = 0.0;
+
+		// Generate Ray direction vectors
+		rays[ray_no].theta = 2 * (float)PI * uniform_random(generator);
+		rays[ray_no].phi = (float)acos(2 * uniform_random(generator) - 1.0);
+
+		rays[ray_no].direction.x = cos(rays[ray_no].theta) * sin(rays[ray_no].phi);
+		rays[ray_no].direction.y = sin(rays[ray_no].theta) * sin(rays[ray_no].phi);
+		rays[ray_no].direction.z = cos(rays[ray_no].phi);
+
+	}
+
+	cout << "Rays vectors generated \n";
+}
+
+void source::reset_rays(void) {
+	unsigned int ray_no;
+
+	for (ray_no = 0; ray_no < number_of_rays; ray_no++) {
+		rays[ray_no].isource_position = this->position;
+		rays[ray_no].R_eff.real = 1;
+		rays[ray_no].R_eff.imaginary = 0;
+
+		rays[ray_no].direction.x = cos(rays[ray_no].theta) * sin(rays[ray_no].phi);
+		rays[ray_no].direction.y = sin(rays[ray_no].theta) * sin(rays[ray_no].phi);
+		rays[ray_no].direction.z = cos(rays[ray_no].phi);
+	}
+}
+
+void source::delete_source(void) {
+	delete[] rays;
+	source_count--;
+}
+
+source::source(void)
+{
+	source_count++;
+	cout << "Source Created \n";
+}
+
+source::~source(void)
+{
+}
+
+class receiver
+{
+public:
+	vec3 position;
+	double radius;
+	unsigned long int detect_count;
+	complex pressure;
+
+	void reset(void);
+};
+
+void receiver::reset(void)
+{
+	pressure.real = 0;
+	pressure.imaginary = 0;
+	detect_count = 0;
+}
+
+
+// -----------------Function Definations --------------------- 
+
+double dotProduct(vec3 vect_A, vec3 vect_B)
+{
+	double dot_product = 0;
+	dot_product = vect_A.x * vect_B.x + vect_A.y * vect_B.y + vect_A.z * vect_B.z;
+	return dot_product;
+}
+
+vec3 crossProduct(vec3 vect_A, vec3 vect_B)
+{
+	vec3 cross_product;
+
+	cross_product.x = vect_A.y * vect_B.z - vect_A.z * vect_B.y;
+	cross_product.y = vect_A.z * vect_B.x - vect_A.x * vect_B.z;
+	cross_product.z = vect_A.x * vect_B.y - vect_A.y * vect_B.x;
+
+	return cross_product;
+}
+
+double mod_vect(vec3 vect)
+{
+	double mod;
+	mod = sqrt(pow(vect.x, 2) + pow(vect.y, 2) + pow(vect.z, 2));
+	return mod;
+}
+
+plane generate_plane(vec3 point1, vec3 point2, vec3 point3)
+{
+	plane plane;
+	
+	double mod_N;
+	double D;
+
+	vec3 v1, v2;
+	vec3 N, n;
+
+	v1 = point3 - point2;
+
+	v2 = point1 - point2;
+
+	N = crossProduct(v1, v2);
+
+	mod_N = mod_vect(N);
+
+	n = N / mod_N;
+
+	D = -1.0 * dotProduct(n, point1);
+
+	plane.normal.x = n.x;
+	plane.normal.y = n.y;
+	plane.normal.z = n.z;
+	plane.D = D;
+
+	return plane;
+}
+
+vec3 image_source_position(vec3 source_position, plane plane)
+{
+	vec3 image_source_position;
+
+	double dist_along_normal = dotProduct(plane.normal, source_position) + plane.D; 
+
+	image_source_position = source_position - (plane.normal * (2 * dist_along_normal));
+
+	return image_source_position;
+}
+
+vec3 reflection_point(vec3 source_position, plane plane, ray current_ray)
+{
+	vec3 reflection_point;
+
+	double dist_along_normal = dotProduct(plane.normal, source_position) + plane.D;
+
+	double dist_along_ray = (-1.0 * dist_along_normal) / dotProduct(current_ray.direction, plane.normal);
+
+	reflection_point = source_position + (current_ray.direction * dist_along_ray);
+
+	return reflection_point;
+}
+
+double reflection_angle(plane plane, ray current_ray) 
+{
+	double theta;
+
+	theta = acos(-1.0 * dotProduct(plane.normal, current_ray.direction));
+
+	return theta;// In Radians
+}
+
+vec3 reflected_ray(plane plane, ray current_ray)
+{
+	vec3 reflected_ray;
+
+	reflected_ray = current_ray.direction - (plane.normal * (2.0 * dotProduct(plane.normal, current_ray.direction)));
+
+	return reflected_ray;
+}
+
+bool check_reflecting_plane(vec3 source_position, plane plane)
+{
+	bool reflecting = 0;
+
+	double dist_along_normal = dotProduct(plane.normal, source_position) + plane.D;
+
+	if (dist_along_normal <= 0)
+	{
+		reflecting = false;
+	}
+	else if (dist_along_normal > 0)
+	{
+		reflecting = true;
+	}
+
+	return reflecting;
+}
+
+int find_reflecting_box_plane(box room, source source, int ray_no)
+{
+	int no_of_planes = 6;
+	int reflecting_plane_no=-1;
+
+	// Reset plane labels
+	for (int j = 0; j < no_of_planes; j++)
+	{
+		room.box_plane[j].label = 0;
+	}
+
+	double min_dist = 1.0e8;//initialise with bad value 
+
+	for (int j = 0; j < no_of_planes; j++)
+	{
+		bool reflecting = check_reflecting_plane(source.rays[ray_no].isource_position, room.box_plane[j]);
+
+		if (reflecting)
+		{
+			room.box_plane[j].label = 1;
+		}
+		
+		if (room.box_plane[j].label)
+		{
+			vec3 normal, isource_position, ray_direction;
+			normal = room.box_plane[j].normal;
+			isource_position = source.rays[ray_no].isource_position;
+			ray_direction = source.rays[ray_no].direction;
+
+			// Get distance of plane along ray direction
+			double dist_along_normal = dotProduct(normal, isource_position) + room.box_plane[j].D;
+			double dist_along_ray = (-1.0 * dist_along_normal) / dotProduct(ray_direction, normal);
+
+			if (dist_along_ray <= min_dist && dist_along_ray > 0)
+			{
+				// Get plane with minimum distance and retrieve plane number for that plane
+				min_dist = dist_along_ray;
+				reflecting_plane_no = j;
+			}
+		}
+	}
+
+	if (reflecting_plane_no == -1) cout << "Error : Can't find reflecting plane \n Source and receiver need to be inside the room \n";
+
+	return reflecting_plane_no;
+}
+
+
+complex calculate_ray_pressure(source source, receiver receiver, int ray_no) 
+{
+	double path_length = mod_vect(receiver.position - source.rays[ray_no].isource_position);
+
+	double D_ray = 2 * sqrt(pow(receiver.radius, 2) - pow(mod_vect(source.rays[ray_no].calc_pt - receiver.position), 2));
+
+	double V = (4 / 3)*PI*pow(receiver.radius, 3);
+
+	double L = sqrt(PI * pow(receiver.radius, 2));
+
+	double rho = 1.2041;
+	double c = 343.22;
+
+	double Po = 2 * path_length* ((sqrt(rho*c*source.power*PI*D_ray / V)) / (source.number_of_rays*L));
+
+	complex pressure_ray;
+	//Complex Pressure Function
+	pressure_ray.real = (float)(cos((2 * PI*(path_length)) / source.wavelength)*(Po));
+	pressure_ray.imaginary = (float)(sin((2 * PI*(path_length)) / source.wavelength) * (Po));
+
+	//Multiplication by refflection coefficient
+	pressure_ray = source.rays[ray_no].R_eff*pressure_ray;
+
+	source.rays[ray_no].pressure_square = (pressure_ray.real*pressure_ray.real) + (pressure_ray.imaginary*pressure_ray.imaginary);
+
+	return pressure_ray;
+}
+
+double receiver_pressure_db(receiver receiver)
+{
+	//Convert to Db
+	double p0 = 2.0e-5;
+	double pressure_db = 20 * log10(sqrt((receiver.pressure.real*receiver.pressure.real)+ (receiver.pressure.imaginary*receiver.pressure.imaginary)) / p0);
+	return pressure_db;
+}
+
+complex calculate_R(plane reflecting_plane, double angle_of_reflection)
+{
+	complex R;
+	R= reflecting_plane.R_coeff;
+	return R;
+}
+
+bool detect_receiver(source &source, receiver &receiver, int ray_no)
+{
+	vec3 ray_source = source.rays[ray_no].isource_position;
+	vec3 ray_direc = source.rays[ray_no].direction;
+	vec3 receiver_pos = receiver.position;
+	double receiver_radius = receiver.radius;
+
+	// Find the perpendicular distance of ray from reciever center 
+	double perp_dist;
+	vec3 distance = receiver_pos - ray_source;
+
+	double dist_along_ray = dotProduct(distance, ray_direc);
+
+	if (dist_along_ray < 0) return false;
+
+	// Find point along direction of ray with dotproduct magnitude 
+	vec3 test_pt = ray_source + ray_direc * dist_along_ray;
+
+	// Evaluate the distance of that point with reciever position
+	perp_dist = sqrt(pow((test_pt.x - receiver_pos.x), 2) + pow((test_pt.y - receiver_pos.y), 2) + pow((test_pt.z - receiver_pos.z), 2));
+ 
+	if (perp_dist < receiver_radius && perp_dist >= 0){
+		//Update Test Point for ray
+		source.rays[ray_no].calc_pt = test_pt;
+		receiver.detect_count++;
+		return true;
+	}
+	else{
+		//Clear calc_pt as ray is not detected reciever
+		source.rays[ray_no].calc_pt.x = 0;
+		source.rays[ray_no].calc_pt.y = 0;
+		source.rays[ray_no].calc_pt.z = 0;
+		return false;
+	}
+}
